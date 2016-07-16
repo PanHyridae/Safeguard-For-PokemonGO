7c6f52c5182658130e10ac7d95470333cc7dd3df
Marlon Jones <marlonjones234@gmail.com>
2016-07-16 17:00:21 -0500

    Test Notification

--- app/src/main/java/com/marlonjones/safeguard/MainActivity.java
+++ app/src/main/java/com/marlonjones/safeguard/MainActivity.java
@@ -1,10 +1,15 @@
 package com.marlonjones.safeguard;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -15,7 +20,7 @@ import android.widget.Button;
 import com.afollestad.materialdialogs.MaterialDialog;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final int NOTIFICATION_ID = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //If Pokemon GO is installed, you can open it with this button!
-                boolean installed = appcheck ("com.nianticlabs.pokemongo");
+                boolean installed = appcheck("com.nianticlabs.pokemongo");
                 if (installed) {
                     Intent pokemon = getPackageManager()
                             .getLaunchIntentForPackage("com.nianticlabs.pokemongo");
@@ -55,6 +60,50 @@ public class MainActivity extends AppCompatActivity {
                             .positiveText(R.string.button_ok)
                             .show();
                 }
+            }
+        });
+
+        //This button tests the Dialog portion of the warnings
+        Button buttonTestDialog = (Button) findViewById(R.id.buttontest);
+        buttonTestDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new MaterialDialog.Builder(MainActivity.this)
+                        .title(R.string.test)
+                        .content(R.string.dialog_test)
+                        .positiveText(R.string.button_ok)
+                        .show();
+            }
+        });
+        //This button does two things. If the user is running Android Lollipop,
+        //Marshmallow, Nougat, or anything above that uses the Heads-Up Notifications,
+        //then they will see a notification. If not, they will see a dialog! :D
+        //-------------------------------------------------------------------------
+        Button buttonTestNote = (Button) findViewById(R.id.buttontest2);
+        buttonTestNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Lollipop or Above
+                if (Build.VERSION.SDK_INT >= 21) {
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this);
+                            new NotificationCompat.Builder(MainActivity.this);
+                                    builder.setSmallIcon(R.drawable.smallplaceholder);
+                                    builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+                                    builder.setContentTitle("Test Notification");
+                                    builder.setContentText("This is a test of the Heads-Up notification! You will see this every 15 minutes when playing Pokemon GO!");
+                                    builder.setPriority(Notification.PRIORITY_HIGH);
+                                    builder.setDefaults(Notification.DEFAULT_VIBRATE);
+                                    NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                                    notificationManager.notify(NOTIFICATION_ID, builder.build());
+                    //Below Lollipop
+                } else {
+                    new MaterialDialog.Builder(MainActivity.this)
+                            .title(R.string.error_dialog_title)
+                            .content(R.string.notification_test_error)
+                            .positiveText(R.string.button_ok)
+                            .show();
+                }
+
             }
         });
     }
